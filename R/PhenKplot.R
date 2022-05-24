5c478976a3fdea6522254c2578c99e35c7bb3a76
José A. Lastra <jose.lastra@pucv.cl>
2022-05-24 09:40:02 +0000

    version 1.5.0

--- R/PhenKplot.R
+++ R/PhenKplot.R
@@ -1,77 +1,83 @@
 #' @title PhenKplot
 #' @description Plot the most probable vegetation greenness values.
 #' @encoding UTF-8
-#' @param x	Numeric vector with greenness values
-#' @param dates	Vector with dates at which the greenness values were recorded
-#' @param h	Numeric. Indicates the geographic hemisphere to define the starting date of the growing season. h=1 if the vegetation is in the Northern Hemisphere (season starting at January 1st), h=2 if it is in the Southern Hemisphere (season starting at July 1st)
-#' @param nGS Numeric. Number of greenness values within a single growing season. For example, nGS=23 for MODIS	Vegetation Index 16-days composites
+#' @param x Numeric vector. A time series of a vegetation index (e.g. LAI, NDVI, EVI) or any other variable with seasonal behavior. The code has been optimized to work with integer values. Please re-scale the input values if necessary (e.g. NDVI ranging from 0.0000 to 1.0000, multiply by 10,000
+#' @param dates A date vector. The number of dates must be equal to the number of "x" values (numeric input vector).
+#' @param h Numeric. Indicates the geographic hemisphere to define the starting date of the growing season. h=1 if the vegetation is in the Northern Hemisphere (season starting at January 1st), h=2 if it is in the Southern Hemisphere (season starting at July 1st)
 #' @param xlab Character vector (or expression) giving plot title in x axis label
-#' @param ylab Caracter vector (or expression) giving plot title in y axis label
-#' @param rge  A vector containing minimum and maximum values of the response variable used in the analysis. We suggest the use of theoretically based limits. For example in the case of MODIS NDVI or EVI, it ranges from 0 to 10,000, so rge =c(0,10000)
-#' @details It is the graphical version of the Phen function. It calculates and plot a likelihood map of the vegetation-greenness – time space using a numeric vector of vegetation canopy greenness values (e.g. Leaf Area Index (LAI) or greenness proxies values such as the Normalized Difference Vegetation Index (NDVI) or Enhanced Vegetation Index (EVI). Also a vector with dates for the greenness values is required. This function calculates the confidence areas on a per year basis. Functions for confidence intervals on per day basis are under development. This function is partially based on the ci2d function on package gplots.
-#' @seealso \code{\link{Phen}}
+#' @param ylab Character vector (or expression) giving plot title in y axis label
+#' @param rge  Numeric vector with two values setting the minimum and maximum values of the response variable (e.g. NDVI) used in the analysis. We suggest the use of theoretically based limits. For example in the case of MODIS NDVI or EVI, it ranges from 0 to 10,000, so rge =c(0,10000)
+#' @details It is the graphical version of the \code{\link{Phen}} function. It calculates and plot a likelihood map of the vegetation-greenness – time space using a numeric vector of vegetation canopy greenness values (e.g. Leaf Area Index (LAI) or greenness proxies values such as the Normalized Difference Vegetation Index (NDVI) or Enhanced Vegetation Index (EVI). Also a vector with dates for the greenness values is required. This function calculates the confidence areas on a per year basis. Functions for confidence intervals on per day basis are under development. This function is partially based on the ci2d function on package \href{https://CRAN.R-project.org/package=gplots}{gplots}.
+#' @seealso \code{\link{Phen}}, \code{\link{PhenMap}}
 #' @examples
+#' \dontshow{
+#' ## Testing function with time series of Nothofagus macrocarpa (NDVI)
+#' # Load data
+#' data("phents")
+#' # PhenKplot for the given data
+#' PhenKplot(x=phents$NDVI,dates=phents$dates,h=2,
+#' xlab = "Day of growing season [DGS]",
+#' ylab = "NDVI",rge=c(0,10000))
+#' }
 #' \donttest{
 #' library(lubridate)
 #'
-#' ## Testing North Hemisphere data. Raster data from Slovenia (EVI index), h=1 ##
+#' ## Testing raster data from Central Chile (NDVI), h=2##
 #'
 #' # Load data
-#' sl.path<-system.file("extdata/HN_slovenia",package="npphen")
-#' sl_rasters<-list.files(path=sl.path, pattern=glob2rx("slovenia*.tif"), full.names=TRUE)
-#' Slovenia_rasters<-stack(sl_rasters)
-#' sl_dates<-read.csv(system.file("extdata/date_tables/Slovenia_dates.csv", package="npphen"))
-#' Slovenia_dates <- as.Date(sl_dates$date, format='%d/%m/%Y')
+#' #RasterStack
+#' data("MegaDrought_stack")
+#' #Dates
+#' data("modis_dates")
 #'
-#' # Generate a Raster time series using a raster stack and a date database from Slovenia
+#' # Generate a Raster time series using a raster stack and a date database from Central Chile
 #' # Obtain data from a particular pixel generating a time series
-#' sl_pixel<-cellFromXY(Slovenia_rasters,c(474368,5096979))
-#' sl_pixelts<-as.numeric(Slovenia_rasters[sl_pixel])
-#' plot(Slovenia_dates,sl_pixelts, type='l')
+#' md_pixel <- cellFromXY(MegaDrought_stack,c(313395,6356610))
+#' md_pixelts <- as.numeric(MegaDrought_stack[md_pixel])
+#' plot(modis_dates,md_pixelts, type='l')
 #'
 #' # Phenology for the given pixel
-#' PhenKplot(x=sl_pixelts,dates=Slovenia_dates,h=1,nGS=23, xlab="DOY",
-#' ylab="EVI", rge=c(0,10000))
+#' PhenKplot(x=md_pixelts,dates=modis_dates,h=2,xlab="DGS", ylab="NDVI", rge=c(0,10000))
 #'
 #'
-#' ## Testing South Hemisphere data. Raster data from Chile (EVI index), h=2 ##
+#' ## Testing with the Bdesert_stack from the Atacama Desert, Northern Chile (NDVI), h=2 ##
 #'
 #' # Load data
-#' ay.path<-system.file("extdata/HS_aysen",package="npphen")
-#' ayrasters<-list.files(path=ay.path, pattern=glob2rx("aysen*.tif"), full.names=TRUE)
-#' Aysen_rasters<-stack(ayrasters)
-#' ay_dates<-read.csv(system.file("extdata/date_tables/Aysen_dates.csv", package="npphen"))
-#' Aysen_dates <- as.Date(ay_dates$date, format='%d/%m/%Y')
+#' #RasterStack
+#' data("Bdesert_stack")
+#' #Dates
+#' data("modis_dates")
 #'
-#' # Generate a Raster time series using a raster stack and a date database from Aysen
+#' # Generate a Raster time series using a raster stack and a date database from Northern Chile
 #' # Obtain data from a particular pixel generating a time series
-#' ay_pixel<-cellFromXY(Aysen_rasters,c(228373,4806975))
-#' ay_pixelts<-as.numeric(Aysen_rasters[ay_pixel])
-#' plot(Aysen_dates,ay_pixelts, type = 'l')
+#' bd_pixel<-cellFromXY(Bdesert_stack,c(286638,6852107))
+#' bd_pixelts<-as.numeric(Bdesert_stack[bd_pixel])
+#' plot(modis_dates,bd_pixelts, type = 'l')
 #'
 #' # Phenology for the given pixel
-#' PhenKplot(x=ay_pixelts,dates=Aysen_dates,h=2,nGS=23, xlab="DOY",
-#' ylab="EVI", rge=c(0,10000))
+#' PhenKplot(x=bd_pixelts,dates=modis_dates,h=2,xlab = 'DGS',ylab = 'NDVI',rge=c(0,10000))
 #'}
 #' @export
 
 PhenKplot <-
-  function(x,dates,h,nGS,xlab,ylab,rge) {
+  function(x,dates,h,xlab,ylab,rge) {
 
-    # a.Preparing dataset
     if(length(rge)!=2){stop("rge must be a vector of length 2")}
     if(rge[1]>rge[2]){stop("rge vector order must be minimum/maximum")}
     if(length(dates)!=length(x)){stop("N of dates and files do not match")}
+  
+    nGS <- 365
+   
     if (all(is.na(x))) {
       return(rep(NA,nGS))
     }
     DOY <- yday(dates)
+    DOY[which(DOY==366)]<-365
     D1<-cbind(DOY,x)
-    if(length(unique(D1[,2]))<10 | (nrow(D1)-sum(is.na(D1)))<(0.1*length(D1))) {
+    if(length(unique(D1[,2]))<10 | (nrow(D1)-sum(is.na(D1)))<(0.1*nrow(D1))) { 
       return(rep(NA,nGS))
     }
 
-    # b. Kernel calculation using the reference period (D1)
     if(h!=1 && h!=2){stop("Invalid h")}
     DOGS<-cbind(seq(1,365),c(seq(185,365),seq(1,184)))
     if(h==2){
@@ -82,13 +88,23 @@ PhenKplot <-
     Hmat[1,2]<-Hmat[2,1]
     K1<-kde(na.omit(D1),H=Hmat,xmin=c(1,rge[1]),xmax=c(365,rge[2]),gridsize=c(365,500))
     K1Con<-K1$estimate
-    for(j in 1:365){
-      K1Con[j,]<-K1$estimate[j,]/sum(K1$estimate[j,])}
+    
+    for(j in 1:365){ 
+      Kdiv<-sum(K1$estimate[j,])
+      ifelse(Kdiv==0,K1Con[j,]<-0,K1Con[j,]<-K1$estimate[j,]/sum(K1$estimate[j,]))}
+    
     MAXY<-apply(K1Con,1,max)
-    for(i in 1:365){
-      MAXY[i]<-median(K1$eval.points[[2]][which(K1Con[i,]==MAXY[i],arr.ind=TRUE)])}
+    for(i in 1:365){ 
+      n.select <- which(K1Con[i,]==MAXY[i],arr.ind=TRUE)
+      if(length(n.select) > 1){
+        n <- n.select[1]
+        MAXY[i]<-NA}
+      if(length(n.select) == 1){
+        n <- n.select
+        MAXY[i]<-median(K1$eval.points[[2]][n])
+      }
+    }
 
-    # Plot
     h2d <- list()
     h2d$x <- seq(1,365)
     h2d$y <- seq(rge[1],rge[2],len=500)
@@ -103,3 +119,4 @@ PhenKplot <-
     contour(h2d$x, h2d$y, h2d$cumDensity, levels = c(0,0.5, 0.75, 0.9,0.95),add=T,col=grey(0.25),labcex=1)
     lines(seq(1,365),MAXY,lwd=3,col='dark red')
 }
+
--- R/PhenMap.R
+++ R/PhenMap.R
@@ -1,30 +1,29 @@
 #' @title PhenMap
-#' @description Estimates annual Land Surface Phenology (LSP) using time series of  a vegetation greenness raster stack.
+#' @description Estimates annual Land Surface Phenology (LSP) using time series of a vegetation greenness raster stack.
 #' @encoding UTF-8
-#' @param s	Raster stack with greenness (e,g. NDVI or EV) values
-#' @param dates	Vector with dates at which the greenness values were recorded
-#' @param h	Numeric. Indicates the geographic hemisphere to define the starting date of the growing season. h=1 if the vegetation is in the Northern Hemisphere (season starting at January 1st), h=2 if it is in the Southern Hemisphere (season starting at July 1st)
-#' @param nGS Numeric. Number of greenness values within a single growing season. For example, nGS=23 for MODIS	Vegetation Index 16-days composites
-#' @param nCluster	Numeric. Number of CPU cores to be used for computational calculations
-#' @param outname	Character vector with the output path and filename with extension or only the filename and extension if work directory was set. For example outname="output_phen.tif". See \code{\link{writeRaster}}
-#' @param format	Character. Output file type. See \code{\link{writeFormats}}
-#' @param datatype	Character. Output data type. See \code{\link{dataType}}
+#' @param s Raster stack of a vegetation index (e.g. LAI, NDVI, EVI) or any other variable with seasonal behavior. The code has been optimized to work with integer values. Please re-scale the input raster stack if necessary (e.g. NDVI ranging from 0.0000 to 1.0000, multiply by 10,000
+#' @param dates A date vector. The number of dates must be equal to the number of "x" values (numeric input vector).
+#' @param h Numeric. Indicates the geographic hemisphere to define the starting date of the growing season. h=1 if the vegetation is in the Northern Hemisphere (season starting at January 1st), h=2 if it is in the Southern Hemisphere (season starting at July 1st)
+#' @param frequency Character string. Defines the number of samples for the output phenology and must be one of the this: 'daily' giving output vector of length 365, '8-days' giving output vector of length 46 (i.e MOD13Q1 and MYD13Q1), 'monthly' giving output vector of length 12,'bi-weekly' giving output vector of length 24 (i.e. GIMMS) or '16-days' (default) giving output vector of length 23 (i.e MOD13Q1 or MYD13Q1).
+#' @param nCluster Numeric. Number of CPU cores to be used for computational calculations
+#' @param outname Character vector with the output path and filename with extension or only the filename and extension if work directory was set. For example outname = "output_phen.tif". See \code{\link{writeRaster}}
+#' @param format Character. Output file type. See \code{\link{writeFormats}}
+#' @param datatype Character. Output data type. See \code{\link{dataType}}
 #' @param rge  A vector containing minimum and maximum values of the response variable used in the analysis. We suggest the use of theoretically based limits. For example in the case of MODIS NDVI or EVI, it ranges from 0 to 10,000, so rge =c(0,10000)
-#' @details Derives the annual Land Surface Phenological (LSP) cycle for a standard growing season using a raster stack of satellite based greenness values such as the Normalized Difference Vegetation Index (NDVI) or Enhanced Vegetation Index (EVI). The LSP cycle is calculated for all pixels of the input raster stack in the same way as for the Phen function. The output is a multiband raster where every band is the expected greeness value at a given time step of the standard growing season. For example, for MODIS Vegetation Index 16-days composites the number of time steps of the growing season (nGS) is 23 , and therefore, the output raster will have 23 bands. A vector with dates for the greenness values is also required.
+#' @details Derives the annual Land Surface Phenological (LSP) cycle for a standard growing season using a raster stack of satellite based greenness values such as the Normalized Difference Vegetation Index (NDVI) or Enhanced Vegetation Index (EVI). The LSP cycle is calculated for all pixels of the input raster stack in the same way as for the \code{\link{Phen}} function. The output is a multiband raster where every band is the expected greenness value at a given time step of the standard growing season. For example, for MODIS Vegetation Index 16-days composites the number of time steps of the growing season is 23 (frequency = "16-days"), and therefore, the output raster will have 23 bands. A vector with dates for the greenness values is also required.
 #' @return RasterStack
 #' @seealso \code{\link{Phen}}
 #' @examples
 #' \dontrun{
 #' ##DEPENDING ON HARDWARE, THIS PROCESS CAN BE HIGHLY TIME CONSUMING##
 #'
-#' ## Testing North Hemisphere data. Raster data from Slovenia (EVI index), h=1 ##
+#' ## Testing raster data from Central Chile (NDVI), h=2##
 #'
 #' # Load data
-#' sl.path<-system.file("extdata/HN_slovenia",package="npphen")
-#' sl_rasters<-list.files(path=sl.path, pattern=glob2rx("slovenia*.tif"), full.names=TRUE)
-#' Slovenia_rasters<-stack(sl_rasters)
-#' sl_dates<-read.csv(system.file("extdata/date_tables/Slovenia_dates.csv", package="npphen"))
-#' Slovenia_dates <- as.Date(sl_dates$date, format='%d/%m/%Y')
+#' #RasterStack
+#' data("MegaDrought_stack")
+#' #Dates
+#' data("modis_dates")
 #'
 #' # Making the LSP raster, n bands = 23
 #'
@@ -33,79 +32,128 @@
 #' # Define the number of cores to be use. In this example we use 1
 #' nc1<-1
 #'
-#' PhenMap(s=Slovenia_rasters,dates=Slovenia_dates,h=1,nGS=23, nCluster=nc1,
-#' outname="phen_slov.tif", format="GTiff", datatype="FLT4S",rge=c(0,10000))
-#' #map1<-raster("phen_slov.tif")#run only for load phenology brick
+#'PhenMap(s = MegaDrought_stack,dates = modis_dates,h = 2,
+#'frequency = '16-days',nCluster = nc1,outname = 'phen_MD.tif',
+#'format = 'GTiff',datatype = 'INT2S',rge = c(0,10000))
+#' #map1 <- stack("phen_MD.tif")#run only for load phenology brick
 #' #plot(map1)
 #'
-#' ## Testing South Hemisphere data. Raster data from Chile (EVI index), h=2 ##
+#' ## Testing with the Bdesert_stack from the Atacama Desert, Northern Chile (NDVI), h=2 ##
 #'
 #' # Load data
-#' ay.path<-system.file("extdata/HS_aysen",package="npphen")
-#' ayrasters<-list.files(path=ay.path, pattern=glob2rx("aysen*.tif"), full.names=TRUE)
-#' Aysen_rasters<-stack(ayrasters)
-#' ay_dates<-read.csv(system.file("extdata/date_tables/Aysen_dates.csv", package="npphen"))
-#' Aysen_dates <- as.Date(ay_dates$date, format='%d/%m/%Y')
+#' RasterStack
+#' data("Bdesert_stack")
+#' #Dates
+#' data("modis_dates")
 #'
 #' # Making the LSP raster, n bands = 23
 #' # Define the number of cores to be use. In this example we use 1
 #' nc1<-1
 #'
-#' PhenMap(s= Aysen_rasters,dates=Aysen_dates,h=2,nGS=23, nCluster=nc1,
-#' outname="phen_aysen.tif", format="GTiff", datatype="FLT4S",rge=c(0,10000))
-#' #map2<-raster("phen_aysen.tif") #run only for load phenology brick
+#' PhenMap(s= Bdesert_stack,dates=modis_dates,h=2,
+#' frequency='16-days', nCluster=nc1,outname="phen_BD.tif", 
+#' format="GTiff", datatype="INT2S",rge=c(0,10000))
+#' #map2 <- stack("phen_BD.tif") #run only for load phenology brick
 #' #plot(map2)
 #' }
 #' @export
 
 PhenMap <-
-  function(s,dates,h,nGS,nCluster,outname,format,datatype,rge) {
+  function(s,dates,h,frequency='16-days',nCluster,outname,format,datatype,rge) {
     ff <- function(x) {
-
-      # a.Preparing dataset
+      
       if(length(rge)!=2){stop("rge must be a vector of length 2")}
       if(rge[1]>rge[2]){stop("rge vector order must be minimum/maximum")}
       if(length(dates)!=length(x)){stop("N of dates and files do not match")}
+      if(frequency == 'daily'){
+        nGS <- 365
+      }
+      if(frequency == '8-days'){
+        nGS <- 46
+      }
+      if(frequency == '16-days'){
+        nGS <- 23
+      }
+      if(frequency == 'monthly'){
+        nGS <- 12
+      }
+      if(frequency == 'bi-monthly'){
+        nGS <- 24
+      }
       if (all(is.na(x))) {
         return(rep(NA,nGS))
       }
+      
+      freq.method <- match(frequency, c("daily", "8-days", "16-days", 
+                                         "bi-weekly", "monthly"))
+      if (is.na(freq.method)){ 
+        stop("Invalid frequency. Must be one of: daily, 8-days, 16-days, bi-weekly or monthly")}
+      if (all(is.na(x))) {
+        return(rep(NA,nGS))
+      }
+      
       DOY <- yday(dates)
+      DOY[which(DOY==366)]<-365
       D1<-cbind(DOY,x)
-      if(length(unique(D1[,2]))<10 | (nrow(D1)-sum(is.na(D1)))<(0.1*length(D1))) {
+      if(length(unique(D1[,2]))<10 | (nrow(D1)-sum(is.na(D1)))<(0.1*nrow(D1))) {
         return(rep(NA,nGS))
       }
 
-      # b. Kernel calculation using the reference period (D1)
       if(h!=1 && h!=2){stop("Invalid h")}
       DOGS<-cbind(seq(1,365),c(seq(185,365),seq(1,184)))
       if(h==2){
         for(i in 1:nrow(D1)){
           D1[i,1]<-DOGS[which(DOGS[,1]==D1[i,1],arr.ind=TRUE),2]}}
-
+      
       Hmat<-Hpi(na.omit(D1))
       Hmat[1,2]<-Hmat[2,1]
       K1<-kde(na.omit(D1),H=Hmat,xmin=c(1,rge[1]),xmax=c(365,rge[2]),gridsize=c(365,500))
       K1Con<-K1$estimate
+      
       for(j in 1:365){
-        K1Con[j,]<-K1$estimate[j,]/sum(K1$estimate[j,])}
+        Kdiv<-sum(K1$estimate[j,])
+        ifelse(Kdiv==0,K1Con[j,]<-0,K1Con[j,]<-K1$estimate[j,]/sum(K1$estimate[j,]))}
+      
       MAXY<-apply(K1Con,1,max)
-      for(i in 1:365){
-        MAXY[i]<-median(K1$eval.points[[2]][which(K1Con[i,]==MAXY[i],arr.ind=TRUE)])}
+      for(i in 1:365){ 
+        n.select <- which(K1Con[i,]==MAXY[i],arr.ind=TRUE)
+        if(length(n.select) > 1){
+          n <- n.select[1]
+          MAXY[i]<-NA}
+        if(length(n.select) == 1){
+          n <- n.select
+          MAXY[i]<-median(K1$eval.points[[2]][n])
+        }
+      }
+      
+      if(frequency == 'daily'){
+        select_DGS <- seq(1,365,1)
+        Ref <- MAXY
+      }
+      if(frequency == '8-days'){
+        select_DGS <- seq(1,365,8)
+        Ref <- MAXY[select_DGS]
+      }
+      if(frequency == '16-days'){
+        select_DGS <- seq(1,365,16)
+        Ref <- MAXY[select_DGS]
+      }
+      if(frequency == 'monthly'){
+        select_DGS <- c(15,46,74,105,135,166,196,227,258,288,319,349)
+        Ref <- MAXY[select_DGS]
+      }
+      if(frequency == 'bi-monthly'){
+        select_DGS <- c(1,15,32,46,60,74,91,105,121,135,152,166,182,196,213,227,244,258,274,288,305,319,335,349)
+        Ref <- MAXY[select_DGS]
+      }
+      if(h==1){id.label <- 'DOY'}
+      if(h==2){id.label <- 'DGS'}
 
-      # c. Getting the most probable values for a standard year (nGS time steps)
-      first.DOGS <- which.min(D1[,1])
-      first.DOGS <- first.DOGS[1]
-      last.DOGS <- first.DOGS+nGS-1
-      Ref<-rep(NA,nGS)
-      for(i in first.DOGS:last.DOGS){
-        Ref[i]<-MAXY[D1[i,1]]}
-      #plot(Ref[first.DOGS:last.DOGS],xlab='Time',ylab='Anomaly',font.lab=2,type='l')
-      Ref[first.DOGS:last.DOGS]
+      names(Ref) <- select_DGS 
+      Ref
     }
 
-    #----------------------------------------------------------------------------------------
-    # Calculating the annual phenological curve using n clusters
-    beginCluster(n=nCluster) # write 'beginCluster(n=3)' for using e.g. 3 cores, default uses all available cores)
+    beginCluster(n=nCluster) 
     dates<<-dates
     clusterR(x=s,calc, args=list(ff),export=c('dates'),filename=outname,format=format,datatype=datatype,overwrite=T)
     endCluster()
